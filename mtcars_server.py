9b30606b28967d75a4ae298abdfdb235b0312a16
LuciMcD <lucimcdaniel@gmail.com>
2023-08-04 20:12:31 -0500

    not sure what i'm doing

--- mtcars_server.py
+++ mtcars_server.py
@@ -242,10 +242,10 @@ def get_mtcars_server_functions(input, output, session):
     @render_widget
     def mtcars_stock_chart():
         df = get_mtcars_price_df()
-        df_stock = df[df["Company"] == reactive_stock.get()]
-        logger.info(f"Rendering Stock chart with {len(df_stock)} bars")
+        df_company = df[df["Company"] == reactive_stock.get()]
+        logger.info(f"Rendering Price chart with {len(df_company)} bars")
         plotly_express_plot = px.bar(
-            df_stock, x='TIME.index', y="Price.columns", color="Company"
+            df_company, x="Time", y="Price", color="Company"
         )
         plotly_express_plot.update_layout(title="Continuous Stock Prices")
         return plotly_express_plot
