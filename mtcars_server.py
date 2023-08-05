85ed97bb7e8e913fc8cf3bccee3e188233b81458
LuciMcD <lucimcdaniel@gmail.com>
2023-08-04 19:31:01 -0500

    x=ticker

--- mtcars_server.py
+++ mtcars_server.py
@@ -245,7 +245,7 @@ def get_mtcars_server_functions(input, output, session):
         df_stock = df[df["Company"] == reactive_stock.get()]
         logger.info(f"Rendering Stock chart with {len(df_stock)} bars")
         plotly_express_plot = px.bar(
-            df_stock, x="TIME", y="Price", color="Company"
+            df_stock, x="Ticker", y="Price", color="Company"
         )
         plotly_express_plot.update_layout(title="Continuous Stock Prices")
         return plotly_express_plot
